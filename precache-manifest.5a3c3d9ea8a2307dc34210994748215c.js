96971234ba9190e85d07592fd93408e7edb2f0c2
rkbhambri <ramkrishan@redbaron.in>
2020-06-22 07:36:15 +0530

    Updates

---
+++ precache-manifest.5a3c3d9ea8a2307dc34210994748215c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95283b627844c09b0364e814c39a56be",
+    "url": "/symphony-ui/index.html"
+  },
+  {
+    "revision": "eecae602ded802f29566",
+    "url": "/symphony-ui/static/css/main.3c05a7cb.chunk.css"
+  },
+  {
+    "revision": "f96e70d64945b74565ed",
+    "url": "/symphony-ui/static/js/2.0688a84c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/symphony-ui/static/js/2.0688a84c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eecae602ded802f29566",
+    "url": "/symphony-ui/static/js/main.da5349de.chunk.js"
+  },
+  {
+    "revision": "e9fc9fc080c7650a52fc",
+    "url": "/symphony-ui/static/js/runtime-main.fc4c7a30.js"
+  }
+]);
